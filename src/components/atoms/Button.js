63ee67162f58d422e9a8b1c099699d81b6e5fee1
hw0k <hw0knam@gmail.com>
2019-10-21 19:51:45 +0900

    add: add button

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+import COLORS from 'assets/colors';
+
+const Button = styled.button`
+  font-size: 1.25rem;
+  padding-left: 0.8rem;
+  padding-right: 0.8rem;
+  padding-top: 0.4rem;
+  padding-bottom: 0.4rem;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.4s;
+  ${props => props.inverted && props.inverted === true
+    ? css`
+      background-color: ${props.color};
+      :hover {
+        background-color: ${COLORS.WHITE};
+        border: 1px solid ${props.color};
+        color: ${props.color};
+      }
+    ` : css`
+      border: 1px solid ${props.color};
+      color: ${props.color};
+      :hover {
+        background-color: ${props.color};
+        color: ${COLORS.WHITE};
+      }
+    `
+  }
+`;
+
+export default Button;
