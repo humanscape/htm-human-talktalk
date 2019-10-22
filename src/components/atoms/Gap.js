9e9e7eb8a171949e8cbadbcf2ccf69c017e1fdb3
hw0k <hw0knam@gmail.com>
2019-10-22 12:53:46 +0900

    fix: fix margin not worked properly

--- src/components/atoms/Gap.js
+++ src/components/atoms/Gap.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 const Gap = styled.div`
   ${props => props.top && `margin-top: ${props.top};`}
-  ${props => props.left && `margin-top: ${props.left};`}
-  ${props => props.right && `margin-top: ${props.right};`}
-  ${props => props.bottom && `margin-top: ${props.bottom};`}
+  ${props => props.left && `margin-left: ${props.left};`}
+  ${props => props.right && `margin-right: ${props.right};`}
+  ${props => props.bottom && `margin-bottom: ${props.bottom};`}
 `;
 
 export default Gap;
